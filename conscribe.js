6552044564662ad6de2edbbef20200a9fcdbad7d
johnsonnc <nicholas.c.j@gmail.com>
2013-10-02 23:07:30 -0400

    Added missing files

--- conscribe.js
+++ conscribe.js
@@ -1,1 +1,21 @@
 /**
+ * Conscribe.js
+ * Used to merge JSON Schema(ish) files with MD
+ */
+
+var fs = require("fs");
+var path = require("path");
+
+// read in JSON file...
+var file = __dirname + '/test/schema.json';
+
+fs.readFile(file, 'utf8', function (err, data) {
+  if (err) {
+    console.log('Error: ' + err);
+    return;
+  }
+
+  data = JSON.parse(data);
+
+  console.dir(data);
+});
