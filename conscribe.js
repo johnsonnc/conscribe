bed72fb589efca26ee063b9b0dcc8f735b6c3c48
johnsonnc <nicholas.c.j@gmail.com>
2013-10-03 22:56:10 -0400

    Version 1 Complete

--- conscribe.js
+++ conscribe.js
@@ -4,42 +4,111 @@
  */
 
 var fs = require("fs");
-var path = require("path");
-var schmea;
+
+var schema;
 // read in JSON file...
 var schema_file = __dirname + '/test/schema.json';
+var examples;
+var data = fs.readFileSync(schema_file, 'utf8');
 
-fs.readFile(schema_file, 'utf8', function (err, data) {
-  if (err) {
-    console.log('Error: ' + err);
-    return;
-  }
-
-  schema = JSON.parse(data);
-
-  
-});
-
-var examples_files = __dirname + '/text/examples.md';
-
-fs.readFile(examples_file,'utf8', function(err,data){
-
-if (err) {
-    console.log('Error: ' + err);
-    return;
-  }
-
-  examples = JSON.parse(data);
-});
+ schema = JSON.parse(data);
 
 
-for(var i in schema.schema){
-	schem = schema.schema;
 
-	var composedString;
 
-	composedString = "##" + i "\n\n";
-	composedString = "Purpose : " + schem[i].purpose + "\n\n";
-	composedString = "";
-	examples.replace('/<' + i + '>/g', composedString);
+var examples_files = __dirname + '/test/examples.md';
+ 
+examples = fs.readFileSync(examples_files,'utf8');
+
+var output = examples;
+ 
+var schem = schema.channels;
+
+
+
+var pDCount = 0;
+function propertiesDescptRecursive(args,head){
+
+	var r = "";
+	var required;
+	for (var i in args){
+		if(args[i].required === true){
+			required = "required";
+		}else{
+			required = "optional";
+		}
+		r += "**" + head + "." + i + "** : *("+ required + ", " + args[i].type +")* " + args[i].desc + "\n\n";
+		if(args[i].properties !== undefined && args[i].properties !==null){
+			r += propertiesDescptRecursive(args[i].properties,head+"."+i);
+		}
+	}
+	return r;
 }
+
+function propertiesRecursive(args,head){
+	pDCount = pDCount +  1;
+	var count = pDCount;
+	var r = "";
+	var tab = "\t\t";
+	while(count > 0){
+		
+		tab += "\t";
+		count =count -1;
+	}
+
+	var required;
+	for (var i in args){
+		if(args[i].required === true){
+			required = "required";
+		}else{
+			required = "optional";
+		}
+		r += tab + i + " : "+ required + ", " + args[i].type +", " + "\n";
+		if(args[i].properties !== undefined && args[i].properties !==null){
+			r += "{\n";
+			r += propertiesRecursive(args[i].properties,head+"."+i);
+			r+= tab+"\n},";
+		}
+	}
+	pDCount = pDCount -1;
+	return r;
+}
+
+for(var i in schem){
+
+	var composedString = "";
+	var tempString = "";
+	composedString += "## " + i + "\n\n";
+	composedString += "**Purpose** : " + schem[i].purpose + "\n\n";
+	composedString += "**Response Channel** : " + schem[i].resultChannel + "\n\n";
+	composedString += "**Payload** : \n\n";
+	composedString += "\t {\n";
+	for (var x in schem[i].payload){
+		composedString += "\t\t" + x + " : ";
+		if(schem[i].payload[x].properties !== undefined && schem[i].payload[x].properties !== null){
+			composedString += "{\n";
+			composedString += propertiesRecursive(schem[i].payload[x].properties, x);
+			composedString += "\t\t} ";
+		}
+
+		if(schem[i].payload[x].required === true){
+			var required = "required";
+		}else{
+			var required = "optional";
+		}
+		composedString += required + ", " + schem[i].payload[x].type + "\n";
+
+
+		tempString += "**"+ x + "** : " + "*("+required+", "+schem[i].payload[x].type+")* " + schem[i].payload[x].description+"\n\n";
+		if(schem[i].payload[x].properties !== undefined && schem[i].payload[x].properties !== null){
+			tempString += propertiesDescptRecursive(schem[i].payload[x].properties, x);
+		}
+	}
+	composedString += "\t }\n\n";
+	composedString += tempString;
+	var searchString = "<" +i + ">";
+	output = output.replace(searchString, composedString);
+}
+//
+//console.dir(output);
+fs.writeFileSync("output.md",output);
--- test/examples.md
+++ test/examples.md
@@ -1,13 +1,369 @@
 
-<exampleSchema>
+<map.overlay.create> 
 
-## Example 1
+### Example 1
 
 	{
 		//this would be an example.
 	}
 
-## Example 2
+### Example 2
+
+	{	}
+
+Caption of Example 2
+
+
+
+<map.overlay.remove> 
+
+### Example 1
+
+	{
+		//this would be an example.
+	}
+
+### Example 2
+
+	{	}
+
+Caption of Example 2
+
+
+<map.overlay.hide> 
+
+### Example 1
+
+	{
+		//this would be an example.
+	}
+
+### Example 2
+
+	{	}
+
+Caption of Example 2
+
+
+
+<map.overlay.show> 
+
+### Example 1
+
+	{
+		//this would be an example.
+	}
+
+### Example 2
+
+	{	}
+
+Caption of Example 2
+
+
+<map.overlay.update> 
+
+### Example 1
+
+	{
+		//this would be an example.
+	}
+
+### Example 2
+
+	{	}
+
+Caption of Example 2
+
+
+<map.feature.plot> 
+### Example 1
+
+	{
+		//this would be an example.
+	}
+
+### Example 2
+
+	{	}
+
+Caption of Example 2
+
+
+<map.feature.plot.url> 
+### Example 1
+
+	{
+		//this would be an example.
+	}
+
+### Example 2
+
+	{	}
+
+Caption of Example 2
+
+
+<map.feature.unplot> 
+### Example 1
+
+	{
+		//this would be an example.
+	}
+
+### Example 2
+
+	{	}
+
+Caption of Example 2
+
+
+<map.feature.hide> 
+### Example 1
+
+	{
+		//this would be an example.
+	}
+
+### Example 2
+
+	{	}
+
+Caption of Example 2
+
+
+<map.feature.show> 
+### Example 1
+
+	{
+		//this would be an example.
+	}
+
+### Example 2
+
+	{	}
+
+Caption of Example 2
+
+
+<map.feature.selected> 
+### Example 1
+
+	{
+		//this would be an example.
+	}
+
+### Example 2
+
+	{	}
+
+Caption of Example 2
+
+
+<map.feature.update> 
+### Example 1
+
+	{
+		//this would be an example.
+	}
+
+### Example 2
+
+	{	}
+
+Caption of Example 2
+
+
+<map.view.zoom> 
+### Example 1
+
+	{
+		//this would be an example.
+	}
+
+### Example 2
+
+	{	}
+
+Caption of Example 2
+
+
+<map.view.center.overlay> 
+### Example 1
+
+	{
+		//this would be an example.
+	}
+
+### Example 2
+
+	{	}
+
+Caption of Example 2
+
+
+<map.view.center.feature> 
+### Example 1
+
+	{
+		//this would be an example.
+	}
+
+### Example 2
+
+	{	}
+
+Caption of Example 2
+
+
+<map.view.center.location> 
+### Example 1
+
+	{
+		//this would be an example.
+	}
+
+### Example 2
+
+	{	}
+
+Caption of Example 2
+
+
+<map.view.center.bounds> 
+### Example 1
+
+	{
+		//this would be an example.
+	}
+
+### Example 2
+
+	{	}
+
+Caption of Example 2
+
+
+<map.view.clicked> 
+### Example 1
+
+	{
+		//this would be an example.
+	}
+
+### Example 2
+
+	{	}
+
+Caption of Example 2
+
+
+<map.get> 
+### Example 1
+
+	{
+		//this would be an example.
+	}
+
+### Example 2
+
+	{	}
+
+Caption of Example 2
+
+
+<map.status.request> 
+### Example 1
+
+	{
+		//this would be an example.
+	}
+
+### Example 2
+
+	{	}
+
+Caption of Example 2
+
+
+<map.status.initialization> 
+### Example 1
+
+	{
+		//this would be an example.
+	}
+
+### Example 2
+
+	{	}
+
+Caption of Example 2
+
+
+<map.status.view> 
+### Example 1
+
+	{
+		//this would be an example.
+	}
+
+### Example 2
+
+	{	}
+
+Caption of Example 2
+
+
+<map.status.format> 
+### Example 1
+
+	{
+		//this would be an example.
+	}
+
+### Example 2
+
+	{	}
+
+Caption of Example 2
+
+
+<map.status.about> 
+### Example 1
+
+	{
+		//this would be an example.
+	}
+
+### Example 2
+
+	{	}
+
+Caption of Example 2
+
+
+<map.error> 
+### Example 1
+
+	{
+		//this would be an example.
+	}
+
+### Example 2
+
+	{	}
+
+Caption of Example 2
+
+
+<transaction.complete> 
+### Example 1
+
+	{
+		//this would be an example.
+	}
+
+### Example 2
 
 	{	}
 
